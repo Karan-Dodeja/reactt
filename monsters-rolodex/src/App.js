bc0ace9bdd0d36b29eab787808d27dada30e4c0d
KaranDodeja03 <karandodeja03@gmail.com>
2023-03-07 23:09:09 +0530

    Optimizing Search Functionality

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,21 +1,15 @@
 import { Component } from 'react';
-
 import './App.css';
 
 class App extends Component {
-
   constructor() {
     console.log('constructor Call')
     super();
-
     this.state = {
-
       monsters: [],
       searchField: ''
     };
-
   }
-
   componentDidMount() {
     console.log('fetch Call')
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -26,38 +20,35 @@ class App extends Component {
       ))
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField }
+    },
+    )
+  }
+
   render() {
     console.log('render Call')
 
-    const filterMethod = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    
+    const filterMethod = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
-
+    
     return (
       <div className="App">
-        <input type="search" className="searchBox" placeholder='Searh Monsters' onChange={(event) => 
-        {
-        
-          const searchField = event.target.value.toLocaleLowerCase();
-    
-          this.setState(() => {
-            return { searchField }
-          },
-            
-          )
-        
-        }
-        
-        } />
+        <input type="search" className="searchBox" placeholder='Searh Monsters' onChange={onSearchChange} />
         {
           filterMethod.map((monster) => {
             return <h1 key={monster.id}>{monster.name}</h1>;
           })
         }
-
       </div >
-
     )
+  
   }
 }
 export default App;
