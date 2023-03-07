510f7777cd7ed392249bb6a733ceded16693f53e
KaranDodeja03 <karandodeja03@gmail.com>
2023-03-07 14:55:25 +0530

    setState with Shallow Merge and return function

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,24 +7,32 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name : { firstName: 'Karan', lastName: 'Dodeja'},
+      name: { firstName: 'Karan', lastName: 'Dodeja' },
       company: 'self'
     }
   }
 
   render() {
-return (
-<div className="App">
-      <header className="App-header">
-        <p>
-        Hi {this.state.name.firstName}, {this.state.name.lastName} , Work at {this.state.company}
-        </p>
-        <button onClick={() => {
-          this.setState({name : { firstName: 'Karan 1', lastName: 'Dodeja 1'}})
-        }}>Change Name</button>
-      </header>
-    </div>
-)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Hi {this.state.name.firstName}, {this.state.name.lastName} , Work at {this.state.company}
+          </p>
+          <button onClick={() => {
+            this.setState(
+              () => {
+                return {
+                  name: { firstName: 'Karan 1', lastName: 'Dodeja 1' }
+                };
+              }, () => {
+                console.log(this.state);
+              }
+            )
+          }}>Change Name</button>
+        </header>
+      </div >
+    )
   }
 }
 
