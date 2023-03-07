b66dd74d563a31717b0781b9e9203c9bafa836d4
KaranDodeja03 <karandodeja03@gmail.com>
2023-03-07 21:44:16 +0530

    Search Input Field
    Input Search Box, Storing Data and using filter() & include().

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -5,34 +5,58 @@ import './App.css';
 class App extends Component {
 
   constructor() {
+    console.log('constructor Call')
     super();
 
     this.state = {
 
       monsters: [],
-
+      searchField: ''
     };
 
   }
 
   componentDidMount() {
+    console.log('fetch Call')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) => this.setState(() => {return{monsters: users}
-    },
-    ))
+      .then((users) => this.setState(() => {
+        return { monsters: users }
+      },
+      ))
   }
 
   render() {
+    console.log('render Call')
+
+    const filterMethod = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-
+        <input type="search" className="searchBox" placeholder='Searh Monsters' onChange={(event) => 
         {
-          this.state.monsters.map((monster) => {
+        
+          const searchField = event.target.value.toLocaleLowerCase();
+    
+          this.setState(() => {
+            return { searchField }
+          },
+            
+          )
+        
+        }
+        
+        } />
+        {
+          filterMethod.map((monster) => {
             return <h1 key={monster.id}>{monster.name}</h1>;
           })
         }
+
       </div >
+
     )
   }
 }
