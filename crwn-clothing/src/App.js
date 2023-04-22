1449fd2b5cdb31aee44efb5664efaf816583fe36
kdodeja62@gmail.com <kdodeja62@gmail.com>
2023-04-23 02:00:38 +0530

    Created Categories

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,10 +1,42 @@
 const App = () => {
+
+  const catrgories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ]
+
+
   return (
 
     <div className="categories-container">
-      <div>
-        Hello world
-      </div>
+      {catrgories.map(({ title }) => (
+        <div className="category-container" key="id">
+          <div className="background-image"  />
+          <div className="category-body-container">
+            {/* <img /> */}
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
 
   );
