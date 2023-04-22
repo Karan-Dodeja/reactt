1bb603788ae7d392ad12b8d6d0e7881e79dbce7a
kdodeja62@gmail.com <kdodeja62@gmail.com>
2023-04-22 14:28:48 +0530

    Search Box Component Styles

--- monsters-rolodex/src/components/search-box/search-box.component.jsx
+++ monsters-rolodex/src/components/search-box/search-box.component.jsx
@@ -1,9 +1,13 @@
 import { Component } from "react";
+import './search-box.styles.css'
 
 class searchBox extends Component {
     render() {
         return (
-            <input type="search" className={this.props.className} placeholder={this.props.placeholder} onChange={this.props.onChangeHandler} />
+            <input type="search" 
+            className={`search-box ${this.props.className}` }
+            placeholder={this.props.placeholder} 
+            onChange={this.props.onChangeHandler} />
         )
     }
 }
