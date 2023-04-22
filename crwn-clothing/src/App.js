46e323b380f6ca6f8d367ffde1379c2d975bd34e
kdodeja62@gmail.com <kdodeja62@gmail.com>
2023-04-23 02:55:22 +0530

    Styling Category page using SASS and Image added.

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,25 +1,32 @@
+import './categories.styles.scss';
+
 const App = () => {
 
   const catrgories = [
     {
       id: 1,
       title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png'
     },
     {
       id: 2,
       title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png'
     },
     {
       id: 3,
       title: 'Sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png'
     },
     {
       id: 4,
       title: 'Womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png'
     },
     {
       id: 5,
       title: 'Mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png'
     },
   ]
 
@@ -27,9 +34,12 @@ const App = () => {
   return (
 
     <div className="categories-container">
-      {catrgories.map(({ title }) => (
-        <div className="category-container" key="id">
-          <div className="background-image"  />
+      {catrgories.map(({ title, id, imageUrl }) => (
+        <div className="category-container" key={id}>
+          <div className="background-image" style={{
+            backgroundImage: `url(${imageUrl})`,
+
+          }} />
           <div className="category-body-container">
             {/* <img /> */}
             <h2>{title}</h2>
